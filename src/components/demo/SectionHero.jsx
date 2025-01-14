bd3fd057409d71966f74063f5e98528cb2773efd
Priyanshu Maithani <priyanshumaithani02@gmail.com>
2025-01-14 15:22:21 +0530

    added slider and removed

--- src/components/demo/SectionHero.jsx
+++ src/components/demo/SectionHero.jsx
@@ -1,11 +1,11 @@
 import { HomeSlider } from "./HomeSlider";
-import img1 from "/images/img-1.jpg";
-import img2 from "/images/img-2.jpg";
-import img3 from "/images/img-3.jpg";
+import img1 from "/images/1.png";
+import img2 from "/images/2.png";
+import img3 from "/images/3.png";
 // import img4 from "/images/img-4.jpg";
 // import img5 from "/images/img-5.jpg";
 // import img6 from "/images/img-6.jpg";
-const IMAGES = [img2, img3,img1];
+const IMAGES = [img1, img3,img2];
 export const SectionHero = () => {
   return (
     <>
